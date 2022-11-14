7a4bf33b1df34eff94212c62475655732da234a2
nguyendongminhtri29092014@gmail.com <nguyendongminhtri29092014@gmail.com>
2022-11-14 16:22:43 +0700

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RegisterComponent} from "./form-login/register/register.component";
+import {HomeComponent} from "./home/home/home.component";
+import {ProfileComponent} from "./profile/profile/profile.component";
+import {VideoComponent} from "./profile/video/video.component";
+import {FriendsComponent} from "./profile/friends/friends.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register', component: RegisterComponent},
+  {path: "", component: HomeComponent},
+  {path: 'profile', component: ProfileComponent,
+  children: [
+    {path: 'video', component: VideoComponent},
+    {path: 'friends', component: FriendsComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import { NavBarComponent } from './nav-foot/nav-bar/nav-bar.component';
+import { FooterComponent } from './nav-foot/footer/footer.component';
+import { HomeComponent } from './home/home/home.component';
+import { RegisterComponent } from './form-login/register/register.component';
+import { ProfileComponent } from './profile/profile/profile.component';
+import { VideoComponent } from './profile/video/video.component';
+import { FriendsComponent } from './profile/friends/friends.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent,
+    RegisterComponent,
+    ProfileComponent,
+    VideoComponent,
+    FriendsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatDividerModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
